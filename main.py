3fd44ba7ce36938f4af560a876b7b884d8dce9f5
unknown <arminjafri138386@gmail.com>
2026-05-25 01:41:40 -0700

    final and ready to run

--- main.py
+++ main.py
@@ -4,12 +4,30 @@ import os
 
 load_dotenv()
 api_key = os.getenv("API_KEY")
-l
+
 client = OpenAI(
-    api_key,
+    api_key=api_key,
     base_url="https://openrouter.ai/api/v1"
 )
 
-
 while True:
-    #code inside this loop will keep on running until manually stopped
+    question = input("You: ")
+    print("Thinking...\n")
+
+    response = client.chat.completions.create(
+        model="baidu/cobuddy:free",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a helpful educational tutor."
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    )
+
+    answer = response.choices[0].message.content
+    print("\nAI:", answer)
+    print("\n---------------\n")
